e1fca5b016c2a76d0dec882a0b796656c0915cd4
gtg092x <mdrake@mediadrake.com>
2014-05-03 15:11:04 -0400

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+gulp.task('build', function (cb) {
+
+    console.log('Gulp and running!')
+    process.nextTick(cb);
+});
+
+gulp.task('default', function (cb) {
+
+    console.log('Gulp and running!')
+    process.nextTick(cb);
+});
