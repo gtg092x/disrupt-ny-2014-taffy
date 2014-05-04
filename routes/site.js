0af64131a70ff50f05cff45d4cce4f4242a48acd
gtg092x <mdrake@mediadrake.com>
2014-05-03 20:27:49 -0400

    added weather data

--- routes/site.js
+++ routes/site.js
@@ -5,11 +5,18 @@ function wrap(obj) {
     return {data: obj};
 };
 var consumers = require('../consumers');
+var cache = require('memory-cache');
 module.exports = function (server) {
     /*
      * Brands
      */
     server.get('/trips-summary', function (req, res, next) {
+        var requestHash='/trips-summary'+JSON.stringify(req.params);
+        var cached = cache.get(requestHash);
+        if(cached){
+            res.send(wrap(cached))
+            return next();
+        }
         consumers.concur.trips(req.params, function (err, trips) {
             consumers.aggregate.geocodeAll(trips, function (err, trips) {
                 assert.ifError(err);
@@ -19,6 +26,7 @@ module.exports = function (server) {
                         return b.Segments;
                     }), {TripId: t.TripId, TripName: t.TripName});
                 });
+                cache.put(requestHash,result,1000*60);
                 res.send(wrap(result));
                 return next();
             });
