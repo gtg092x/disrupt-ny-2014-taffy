7c129bfe813465c3e9c36a0369c5d98d808815f8
gtg092x <mdrake@mediadrake.com>
2014-05-04 06:47:11 -0400

    cross domain

--- www/script/app.js
+++ www/script/app.js
@@ -1,5 +1,39 @@
+$("#credits").hide();
+$("#unityPlayer").hide();
+var acted=false;
 $("#get_started").click(function(){
-    $.getJSON("http://disrupt.digitaltaffy.com/trips-summary?format=jsonp&callback=?",function(data){
-        $(document).trigger("unity_start");
+    if(acted)
+    return;
+    acted=true;
+
+    $("<p>Searching for concur data...</p>").appendTo("#status").hide().fadeIn("SLOW",function(){
+        setTimeout(function(){
+
+
+        $("<p>Aggregating esri geo spatial data...</p>").appendTo("#status").hide().fadeIn("SLOW",function(){
+
+        });
+        },2000);
     });
+    $.getJSON("http://disrupt.digitaltaffy.com/trips-summary?format=jsonp&callback=?",function(data){
+        var c=0;
+        data = data.data;
+        for(var i =0;i<data.length;i++)
+            for(var j=0;j<data[i].length;j++)
+                c+=data[i][j].Air?data[i][j].Air.length:0;
+
+        $("<p>Found "+c+" travel legs.</p>").appendTo("#status").hide().fadeIn("SLOW",function(){
+
+        });
+    });
+        setTimeout(function(){
+
+            $("#status").fadeOut(function(){
+                $("#unityPlayer").fadeIn();
+                $(document).trigger("unity_start");
+                $("#credits").fadeIn("SLOW");
+            });
+        },6000);
+
+
 });
