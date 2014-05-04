0e0abeb26d851bba2daa3c066d965bc2af2c99fe
gtg092x <mdrake@mediadrake.com>
2014-05-04 03:53:24 -0400

    codes

--- consumers/concur.js
+++ consumers/concur.js
@@ -66,7 +66,7 @@ var consumer = {
         });
     },
     stuff:function(segments){
-        for(var i = 0;i<10;i++){
+        for(var i = 0;i<20;i++){
             var b = _.cloneDeep(_.sample(segments,1)[0]);
             b.StartCityCode = _.sample(citySamples,1)[0].code;
             b.EndCityCode = _.sample(citySamples,1)[0].code;
